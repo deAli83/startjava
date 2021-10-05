3a78a59792c42dd49ac888ab68787618186c6a2d
Alex D <ali.email@mail.ru>
2021-10-05 22:35:04 +0300

    second homework

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,29 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte core = 4;
+        short hdd = 512;
+        int ram = 6000;
+        long gghz = 2400000;
+        float temp = 37.2f;
+        double inch = 19.2d;
+        char vendor = 'A';
+        boolean notebook = true;
+
+        System.out.print("core = ");
+        System.out.println(core);
+        System.out.print("hdd = ");
+        System.out.println(hdd);
+        System.out.print("ram = ");
+        System.out.println(ram);
+        System.out.print("gghz = ");
+        System.out.println(gghz);
+        System.out.print("temp = ");
+        System.out.println(temp);
+        System.out.print("inch = ");
+        System.out.println(inch);
+        System.out.print("vendor = ");
+        System.out.println(vendor);
+        System.out.print("notebook = ");
+        System.out.println(notebook);
+    }
+}
