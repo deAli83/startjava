3d04b75195ab8b079b10006a3d9d3a31ee345b9a
Alex D <ali.email@mail.ru>
2021-10-12 13:17:32 +0300

    Lesson 2 hw#1

---
+++ lesson 2/Person.java
@@ -0,0 +1,28 @@
+public Person {
+    
+    String sex = "Male";
+    String name = "Vladimir";
+    int height = 175;
+    int weight = 80;
+    int age = 45;
+
+    void move() {
+        System.out.println("moving");
+    }
+
+    void sit() {
+        System.out.println("sitting");
+    }
+
+    void run() {
+        System.out.println("running")
+    }
+
+    void talk() {
+        System.out.println("talking")
+    }
+
+    void studyJava() {
+        System.out.println("studing Java")
+    }
+}
